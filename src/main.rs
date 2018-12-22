e642e021c5b89709cff7d042ed7a4916985f8fa9
Carsten Saathoff <carsten@kodemaniak.de>
2018-12-22 11:31:17 +0100

    Removed "-v" command line flag in favor of "RUST_LOG" environment variable.

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,31 @@
 mod constants;
 
-use clap::{App, Arg, ArgMatches};
-use log::*;
+use clap::{App, Arg};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli_matches = parse_cli();
 
-    init_log(&cli_matches);
+    env_logger::init();
 
     logtopus::run(&cli_matches.value_of("config"))
-}
-
-fn init_log(matches: &ArgMatches) {
-    let loglevel = match matches.occurrences_of("v") {
-        0 => "error",
-        1 => "warn",
-        2 => "info",
-        3 => "debug",
-        _ => "trace",
-    };
-
-    let loglevel = match matches.value_of("module") {
-        Some(module) => {
-            let mut module_loglevel = String::from(module);
-            module_loglevel.push_str("=");
-            module_loglevel.push_str(loglevel);
-            module_loglevel
-        }
-        _ => String::from(loglevel),
-    };
-
-    std::env::set_var("RUST_LOG", &loglevel);
-    env_logger::init();
-    debug!("Setting log level to {}", &loglevel);
 }
 
 fn parse_cli() -> clap::ArgMatches<'static> {
     App::new("logtopus server")
         .version(constants::VERSION)
         .author(constants::AUTHORS)
-        .about("Provides main logtopus server gateway communicating with the tentacles in a cluster.")
-        .arg(Arg::with_name("config")
-            // .required(true)
-            .short("c")
-            .long("config")
-            .value_name("FILE")
-            .help("Sets the configuration file name")
-            .takes_value(true))
-        .arg(Arg::with_name("module")
-            .short("m")
-            .long("module")
-            .takes_value(true)
-            .help("Sets the optional name of the module for which to set the verbosity level"))
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .help("Level of verbosity (error is default) if used multiple times: warn(v), info(vv), debug(vvv) and trace(vvvv)"))
+        .about(
+            "Provides main logtopus server gateway communicating with the tentacles in a cluster.",
+        )
+        .arg(
+            Arg::with_name("config")
+                // .required(true)
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets the configuration file name")
+                .takes_value(true),
+        )
         .get_matches()
 }
