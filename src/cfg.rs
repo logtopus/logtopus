486dbf2e8022673ac51f0e61064fd5af694dfd7a
Carsten Saathoff <carsten@kodemaniak.de>
2019-02-02 13:26:57 +0100

    Default config is now included with include_bytes. Removed several unwraps and properly returning Result now.

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,27 +1,33 @@
 use config;
+use config::ConfigError;
 use std::path::Path;
 
-pub fn read_config<S: AsRef<str>>(maybe_filename: &Option<S>) -> Result<config::Config, String> {
+pub fn read_config<S: AsRef<str>>(
+    maybe_filename: &Option<S>,
+) -> Result<config::Config, config::ConfigError> {
     let mut settings = config::Config::new();
 
-    settings
-        .merge(config::File::with_name("conf/defaults.yml"))
-        .unwrap();
+    let defaults = include_bytes!("default_config.yml");
+    settings.merge(config::File::from_str(
+        String::from_utf8_lossy(defaults).as_ref(),
+        config::FileFormat::Yaml,
+    ))?;
 
     match maybe_filename {
         Some(filename_ref) => {
             let filename = filename_ref.as_ref();
             if !Path::new(filename).exists() {
-                return Err(format!("Configuration file {} does not exist", filename));
+                return Err(ConfigError::Message(format!(
+                    "Configuration file {} does not exist",
+                    filename
+                )));
             } else {
-                settings.merge(config::File::with_name(&filename)).unwrap()
+                settings.merge(config::File::with_name(&filename))?
             }
         }
         None => &settings,
     };
-    settings
-        .merge(config::Environment::with_prefix("app"))
-        .unwrap();
+    settings.merge(config::Environment::with_prefix("app"))?;
 
     Ok(settings)
 }
