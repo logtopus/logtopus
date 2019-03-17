f25fb1c4159ddb1bf76e139598a6fa47df19e97c
Carsten Saathoff <carsten@kodemaniak.de>
2019-03-17 17:51:20 +0100

    Logging level can now be specified by cli parameter similar to tentacle. Fixes #9 for now.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 mod constants;
 
 use clap::{App, Arg};
+use log::*;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli_matches = parse_cli();
 
-    env_logger::init();
+    init_log(&cli_matches);
 
     logtopus::run(&cli_matches.value_of("config"))
+}
+
+fn init_log(matches: &clap::ArgMatches) {
+    let loglevel = match matches.occurrences_of("v") {
+        0 => "error",
+        1 => "warn",
+        2 => "info",
+        3 => "debug",
+        _ => "trace",
+    };
+
+    let loglevel = match matches.value_of("module") {
+        Some(module) => {
+            let mut module_loglevel = String::from(module);
+            module_loglevel.push_str("=");
+            module_loglevel.push_str(loglevel);
+            module_loglevel
+        }
+        _ => String::from(loglevel),
+    };
+
+    std::env::set_var("RUST_LOG", &loglevel);
+    env_logger::init();
+    debug!("Setting log level to {}", &loglevel);
 }
 
 fn parse_cli() -> clap::ArgMatches<'static> {
@@ -27,5 +52,9 @@ fn parse_cli() -> clap::ArgMatches<'static> {
                 .help("Sets the configuration file name")
                 .takes_value(true),
         )
+        .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .help("Level of verbosity (error is default) if used multiple times: warn(v), info(vv), debug(vvv) and trace(vvvv)"))
         .get_matches()
 }
